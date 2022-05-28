71db5c96d3e2e62a2c1415dc26f57319724e114e
"mcIovin" <"60447858+mcIovin@users.noreply.github.com">
2022-05-28 17:24:04 -0500

    Starting with a code base from the OpenZeppelin wizard which has Access Control, and is mintable, pausable, burnable, and enumerable.

---
+++ contracts/AffeMitWaffe.sol
@@ -0,0 +1,54 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts@4.6.0/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts@4.6.0/security/Pausable.sol";
+import "@openzeppelin/contracts@4.6.0/access/AccessControl.sol";
+import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721Burnable.sol";
+
+contract AmWt01 is ERC721, ERC721Enumerable, Pausable, AccessControl, ERC721Burnable {
+    bytes32 public constant PAUSER_ROLE = keccak256("PAUSER_ROLE");
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
+    constructor() ERC721("AmWt01", "AMWT01") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(PAUSER_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "http://amazons.s3.something/";
+    }
+
+    function pause() public onlyRole(PAUSER_ROLE) {
+        _pause();
+    }
+
+    function unpause() public onlyRole(PAUSER_ROLE) {
+        _unpause();
+    }
+
+    function safeMint(address to, uint256 tokenId) public onlyRole(MINTER_ROLE) {
+        _safeMint(to, tokenId);
+    }
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        whenNotPaused
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable, AccessControl)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
