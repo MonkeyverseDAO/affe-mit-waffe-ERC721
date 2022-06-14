1836f0ce40073318310b86f852bb306da14e7331
mcIovin <60447858+mcIovin@users.noreply.github.com>
2022-06-13 22:17:57 -0500

    A couple of minor changes to .gitignore (to ignore a directory where I run some scripts locally), and hardhat config (to add config for deploying to rinkeby and verifying on etherscan.)

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,27 +4,36 @@
 
 require("@nomiclabs/hardhat-ethers");
 require("hardhat-gas-reporter");
+require("@nomiclabs/hardhat-etherscan");
 require('solidity-coverage')
 require('dotenv').config();
-const { COINMARKETCAP_API_KEY } = process.env;
+const { COINMARKETCAP_API_KEY, NODE_PROVIDER_API_URL, PRIVATE_KEY, ETHERSCAN_API_KEY } = process.env;
 
 module.exports = {
-   solidity: {
+    solidity: {
      version:  "0.8.13",
      settings: {
        optimizer: {
          enabled: true,
-         runs: 200
+         runs: 250
        }
-     },
+     }
    },
+   defaultNetwork: "hardhat",
    networks: {
      hardhat: {
        chainId: 1337,
     },
+    rinkeby: {
+      url: NODE_PROVIDER_API_URL,
+      accounts: [`0x${PRIVATE_KEY}`]
+   }
   },
   gasReporter: {
     currency: 'USD',
     coinmarketcap: COINMARKETCAP_API_KEY
+  },
+  etherscan: {
+    apiKey: ETHERSCAN_API_KEY
   }
- };
+};
